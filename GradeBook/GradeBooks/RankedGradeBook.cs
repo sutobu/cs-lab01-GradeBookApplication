9439b7920b18325fe896fdfff5bd7f7a5df54722
maryna.harmeliia <maryna.harmeliia@NY-00001>
2025-03-11 14:22:43 +0100

    override  CalculateStatistics 12 Tests

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -53,5 +53,17 @@ namespace GradeBook.GradeBooks
             }
         }
 
+        public override void CalculateStatistics()
+        {
+           
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
     }
 }
