5a640d2662f7707a27064884282e1909166882a2
maryna.harmeliia <maryna.harmeliia@NY-00001>
2025-03-11 14:17:34 +0100

    override GetLetterGrade 11 Tests

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,13 +16,41 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            //return base.GetLetterGrade(averageGrade);
-            if (Students.Count < 5) 
-            { 
+
+            if (Students.Count < 5)
+            {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             }
-
-
+            var sortedGrades = Students.OrderByDescending(s => s.AverageGrade).ToList();
+            int studentPlace = 1;
+            foreach (var student in sortedGrades)
+            {
+                if (averageGrade < student.AverageGrade)
+                {
+                    studentPlace++;
+                }
+            }
+            double studentPercentage = studentPlace * 100 / (sortedGrades.Count + 1);
+            if (studentPercentage <= 20)
+            {
+                return 'A';
+            }
+            else if (studentPercentage <= 40)
+            {
+                return 'B';
+            }
+            else if (studentPercentage <= 60)
+            {
+                return 'C';
+            }
+            else if (studentPercentage <= 80)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
 
     }
