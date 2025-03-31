ad13d33dd9e369d8b33c03ab3a2746741f1e2e59
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-03-31 15:31:03 +0200

    GPA

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -107,20 +107,35 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double GPA;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    GPA = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    GPA = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    GPA = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    GPA = 2;
+
+                    break;
                 case 'F':
-                    return 0;
+                    GPA = 0;
+                    break;
+                default:
+                    GPA = 0;
+                    break;
             }
-            return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                GPA += 1;
+            }
+            return GPA;
         }
 
         public virtual void CalculateStatistics()
